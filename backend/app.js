31db6202292f66052c6916426f8e4ad7b11046e9
Exequiel <exequiel.miranda245@gmail.com>
2026-03-23 07:30:33 -0600

    inital commit

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import productRoutes from "./src/routes/products.js"; 
+
+//Creo una constante que guarde Express
+const app = express();
+
+//Que acepte los json desde postman
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+
+
+export default app;
